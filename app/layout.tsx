de29162291144e924f07de73f58fd33acc2fe6d3
Amir Kakavand <amirkkvnd99@gmail.com>
2025-02-16 00:12:54 +0330

    Added meeting room controls

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,9 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/toaster"
+// import '@stream-io/video-react-sdk/dist/css/style.css'; <------- This didn't work for some unknown reason
+// So I manually copied everything to this file:
+import "../public/streamStyles/styles.css"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
